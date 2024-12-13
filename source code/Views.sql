ca7f324dd2dedf09e1ffb3439a6b551f540a1b4a
Zahra Hilyatul Jannah <zahra19hilyatul@gmail.com>
2024-12-13 11:23:09 +0700

    update source code

--- source code/Views.sql
+++ source code/Views.sql
@@ -1,30 +1,48 @@
-create or replace PROCEDURE update_data_diri(
-    p_id_customer IN VARCHAR2,
-    p_email IN VARCHAR2,
-    p_password IN VARCHAR2,
-    p_username IN VARCHAR2
-) IS
-    l_customer_exists INTEGER;
-BEGIN
-    -- Check if id_customer exists
-    SELECT COUNT(*)
-    INTO l_customer_exists
-    FROM customer
-    WHERE id_customer = p_id_customer;
+-- PRODUK_TERBARU
+CREATE OR REPLACE FORCE VIEW "SIGMA_USER"."PRODUK_TERBARU" ("ID_PRODUK", "NAMA_PRODUK", "KATEGORI", "DESKRIPSI", "UKURAN", "HARGA_REGULER", "STOK", "DISKON", "NAMA_DISKON", "STOK_TERBARU") AS 
+  SELECT
+    p.id_produk,
+    p.nama_produk,
+    p.kategori,
+    p.deskripsi,
+    dp.ukuran,
+    dp.harga_reguler,
+    dp.stok,
+    d.diskon,
+    d.nama_diskon,
+    dp.stok AS stok_terbaru
+FROM
+    produk p
+JOIN detailproduk dp ON p.id_produk = dp.id_produk
+LEFT JOIN diskon d ON dp.id_diskon = d.id_diskon
+WHERE
+    dp.stok > 0  -- Produk dengan stok lebih besar dari 0
+ORDER BY
+    p.id_produk DESC;
 
-    IF l_customer_exists = 0 THEN
-        RAISE_APPLICATION_ERROR(-20004, 'Error: Customer ID does not exist');
-    END IF;
 
-    -- Update the customer record
-    UPDATE customer
-    SET email = p_email,
-        password = p_password,
-        username = p_username
-    WHERE id_customer = p_id_customer;
-
-    DBMS_OUTPUT.PUT_LINE('Customer data updated successfully for ID: ' || p_id_customer);
-EXCEPTION
-    WHEN OTHERS THEN
-        DBMS_OUTPUT.PUT_LINE('Error updating customer data: ' || SQLERRM);
-END;
+-- RIWAYAT_PEMBELIAN_PENGGUNA
+CREATE OR REPLACE FORCE VIEW "SIGMA_USER"."RIWAYAT_PEMBELIAN_USER" ("ID_CUSTOMER", "USERNAME", "EMAIL", "ID_KERANJANG", "NAMA_PRODUK", "UKURAN", "KUANTITAS", "SUBTOTAL", "ID_CHECKOUT", "TOTAL", "STATUS_PEMBAYARAN", "WAKTU_CHECKOUT", "WAKTU_LAST_UPDATE", "METODE_PEMBAYARAN") AS 
+  SELECT
+    c.id_customer,
+    c.username,
+    c.email,
+    k.id_keranjang,
+    p.nama_produk,
+    dp.ukuran,
+    k.kuantitas,
+    k.subtotal,
+    ch.id_checkout,
+    ch.total,
+    ch.status_pembayaran,
+    ch.waktu_checkout,
+    ch.waktu_last_update,
+    mp.metode_pembayaran
+FROM
+    customer c
+JOIN keranjang k ON c.id_customer = k.id_customer
+JOIN detailproduk dp ON k.id_detail_produk = dp.id_detail_produk
+JOIN produk p ON dp.id_produk = p.id_produk
+JOIN checkout ch ON k.id_checkout = ch.id_checkout
+JOIN metodepembayaran mp ON ch.id_metode = mp.id_metode
+ORDER BY ch.waktu_checkout DESC;
